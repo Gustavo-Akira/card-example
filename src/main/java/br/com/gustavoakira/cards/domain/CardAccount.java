d2f1acf387105784de717b1dbd997785e8ae22fd
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-08-06 20:39:18 -0300

    feat/refactoring: record to class and charge test

--- src/main/java/br/com/gustavoakira/cards/domain/CardAccount.java
+++ src/main/java/br/com/gustavoakira/cards/domain/CardAccount.java
@@ -2,10 +2,72 @@ package br.com.gustavoakira.cards.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record CardAccount(String accountNumber, String cardHolderName, BigDecimal balance, String cardType, String expirationDate) {
+public class CardAccount {
+    private String accountNumber;
+    private String cardHolderName;
+    private BigDecimal balance;
+    private String cardType;
+    private LocalDateTime expirationDate;
+
+    public CardAccount(String accountNumber, String cardHolderName, BigDecimal balance, String cardType, LocalDateTime expirationDate) {
+        this.accountNumber = accountNumber;
+        this.cardHolderName = cardHolderName;
+        this.balance = balance;
+        this.cardType = cardType;
+        this.expirationDate = expirationDate;
+    }
 
     public static CardAccount createAccount(String cardHolderName, BigDecimal initialValue, String type, LocalDateTime expiration) {
+        return new CardAccount(UUID.randomUUID().toString(), cardHolderName, initialValue, type, expiration);
+    }
 
+    public boolean validateCard() {
+        return !this.expirationDate.isBefore(LocalDateTime.now());
+    }
+
+    public void charge(BigDecimal value) {
+        this.balance = this.balance.add(value);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
     }
 }
--- src/test/java/br/com/gustavoakira/cards/domain/CardAccountTest.java
+++ src/test/java/br/com/gustavoakira/cards/domain/CardAccountTest.java
@@ -1,28 +1,44 @@
 package br.com.gustavoakira.cards.domain;
 
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static org.instancio.Select.field;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardAccountTest {
+
+    Model<CardAccount>  validCardAccountModel = Instancio.of(CardAccount.class).set(field("expirationDate"),LocalDateTime.now().plusDays(5)).toModel();
+
+    Model<CardAccount>  invalidCardAccountModel = Instancio.of(CardAccount.class).set(field("expirationDate"),LocalDateTime.now().minusDays(1)).toModel();
+
+
     @Test
     void shouldReturnTrueWhenValidateCardAndCardIsValid(){
 
+        CardAccount account = Instancio.create(validCardAccountModel);
+
+        assertTrue(account.validateCard());
     }
 
     @Test
     void shouldCreateCardAccountWithNumberWhenCallCreateAccount(){
         CardAccount cardAccount = CardAccount.createAccount("", BigDecimal.valueOf(0.0),"black", LocalDateTime.now().plusDays(5));
-        assertNotNull(cardAccount.accountNumber());
+        assertNotNull(cardAccount.getAccountNumber());
         assertNotNull(cardAccount);
     }
 
     @Test
     void shouldChargeWhenCardIsValid(){
-
+        CardAccount cardAccount = Instancio.create(validCardAccountModel);
+        BigDecimal oldValue = cardAccount.getBalance();
+        cardAccount.charge(BigDecimal.TEN);
+        assertEquals(oldValue.add(BigDecimal.TEN), cardAccount.getBalance());
     }
 
     @Test
