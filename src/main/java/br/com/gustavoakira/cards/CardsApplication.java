a1ca6ca7e654948f8fd54a6ee9ab681e1f8ebd82
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-08-10 18:00:11 -0300

    feat: cors with openapi

--- src/main/java/br/com/gustavoakira/cards/CardsApplication.java
+++ src/main/java/br/com/gustavoakira/cards/CardsApplication.java
@@ -1,9 +1,12 @@
 package br.com.gustavoakira.cards;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "default server")})
 public class CardsApplication {
 
 	public static void main(String[] args) {
